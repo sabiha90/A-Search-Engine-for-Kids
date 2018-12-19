ac7d698e323ff9d9fcb7fb93dde45f75fbc4c0ec
Supritha Amudhu <supritha.amudhu@gmail.com>
2018-12-19 01:03:44 -0800

    jQuery DataTable addition

---
+++ app/static/search_engine.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+   var search_results_table = $('#search_results_table').DataTable({
+       // order: [[0, 'desc']]
+       order: [],
+       searching: false
+   });
+});
